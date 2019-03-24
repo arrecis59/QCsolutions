cd0be8875dd2fe7b34277de3e119ac365ba85cc3
santizolisandro <vfernandezs1@miumg.edu.gt>
2019-03-24 14:40:15 -0600

    Se agrego el mantenimiento de Bancos Externos

---
+++ MDI Bancos/BancosExternos/MantenimientoBancosExternos/MantenimientoBancosExternos/ConsultaBancosExternos.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MantenimientoBancosExternos
+{
+    partial class ConsultaBancosExternos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_minimizar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.Lbl_asignacionPerfiles = new System.Windows.Forms.Label();
+            this.dt_BancosExternos = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_BancosExternos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.btn_minimizar);
+            this.panel1.Controls.Add(this.btn_cerrar);
+            this.panel1.Controls.Add(this.Lbl_asignacionPerfiles);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(960, 30);
+            this.panel1.TabIndex = 79;
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_minimizar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_minimizar.FlatAppearance.BorderSize = 0;
+            this.btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_minimizar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_minimizar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btn_minimizar.Location = new System.Drawing.Point(900, 0);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(30, 30);
+            this.btn_minimizar.TabIndex = 5;
+            this.btn_minimizar.Text = "_";
+            this.btn_minimizar.UseVisualStyleBackColor = false;
+            this.btn_minimizar.Click += new System.EventHandler(this.btn_minimizar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btn_cerrar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_cerrar.FlatAppearance.BorderSize = 0;
+            this.btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cerrar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btn_cerrar.Location = new System.Drawing.Point(930, 0);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(30, 30);
+            this.btn_cerrar.TabIndex = 4;
+            this.btn_cerrar.Text = "x";
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // Lbl_asignacionPerfiles
+            // 
+            this.Lbl_asignacionPerfiles.AutoSize = true;
+            this.Lbl_asignacionPerfiles.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_asignacionPerfiles.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Lbl_asignacionPerfiles.Location = new System.Drawing.Point(10, 6);
+            this.Lbl_asignacionPerfiles.Name = "Lbl_asignacionPerfiles";
+            this.Lbl_asignacionPerfiles.Size = new System.Drawing.Size(116, 19);
+            this.Lbl_asignacionPerfiles.TabIndex = 48;
+            this.Lbl_asignacionPerfiles.Text = "Bancos Externos";
+            this.Lbl_asignacionPerfiles.Click += new System.EventHandler(this.Lbl_asignacionPerfiles_Click);
+            // 
+            // dt_BancosExternos
+            // 
+            this.dt_BancosExternos.AllowUserToDeleteRows = false;
+            this.dt_BancosExternos.AllowUserToOrderColumns = true;
+            this.dt_BancosExternos.AllowUserToResizeColumns = false;
+            this.dt_BancosExternos.AllowUserToResizeRows = false;
+            this.dt_BancosExternos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dt_BancosExternos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dt_BancosExternos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dt_BancosExternos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt_BancosExternos.EnableHeadersVisualStyles = false;
+            this.dt_BancosExternos.Location = new System.Drawing.Point(35, 65);
+            this.dt_BancosExternos.Name = "dt_BancosExternos";
+            this.dt_BancosExternos.RowHeadersVisible = false;
+            this.dt_BancosExternos.Size = new System.Drawing.Size(890, 520);
+            this.dt_BancosExternos.TabIndex = 81;
+            this.dt_BancosExternos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dt_BancosExternos_CellContentClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 620);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(960, 30);
+            this.panel2.TabIndex = 82;
+            // 
+            // ConsultaBancosExternos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(60)))), ((int)(((byte)(75)))));
+            this.ClientSize = new System.Drawing.Size(960, 650);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dt_BancosExternos);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ConsultaBancosExternos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ConsultaBancosExternos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_BancosExternos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_minimizar;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.Label Lbl_asignacionPerfiles;
+        private System.Windows.Forms.DataGridView dt_BancosExternos;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ MDI Bancos/BancosExternos/MantenimientoBancosExternos/MantenimientoBancosExternos/Ingreso_de_BancosExternos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDiseno;
+
+namespace MantenimientoBancosExternos
+{
+    public partial class Ingreso_de_BancosExternos : Form
+    {
+        Navegador nv = new Navegador();
+        public Ingreso_de_BancosExternos(DataGridView dgv)
+        {
+            InitializeComponent();
+            nv.nombreForm(this);
+            nv.dgv_datos(dgv);
+
+            nv.ingresarTabla("tbl_bancos_externos");
+        }
+
+        private void txt_Codigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
